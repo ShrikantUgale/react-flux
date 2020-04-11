9195b5b0a42737723d446a06e506a72b6919607c
Shrikant <itzshrikant@gmail.com>
2020-04-11 22:06:20 +0530

    routing added

--- src/components/CourseList.js
+++ src/components/CourseList.js
@@ -1,37 +1,43 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
-const CourseList = (props) => (
-  <table className="table">
-    <thead>
-      <tr>
-        <th>Title</th>
-        <th>Author ID</th>
-        <th>Category</th>
-      </tr>
-    </thead>
-    <tbody>{props.courses.map(renderRow)}</tbody>
-  </table>
-);
-
-const renderRow = (course) => (
-  <tr key={course.id}>
-    <td>{course.title}</td>
-    <td>{course.authorId}</td>
-    <td>{course.category}</td>
-  </tr>
-);
+function CourseList(props) {
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Author ID</th>
+          <th>Category</th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.courses.map(course => {
+          return (
+            <tr key={course.id}>
+              <td>
+                <Link to={"/course/" + course.slug}>{course.title}</Link>
+              </td>
+              <td>{course.authorId}</td>
+              <td>{course.category}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
 
 CourseList.propTypes = {
-  //   courses: PropTypes.array.isRequired,
   courses: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.number.isRequired,
       title: PropTypes.string.isRequired,
       authorId: PropTypes.number.isRequired,
-      category: PropTypes.string.isRequired,
+      category: PropTypes.string.isRequired
     })
-  ).isRequired,
+  ).isRequired
 };
 
 export default CourseList;
--- src/components/CoursesPage.js
+++ src/components/CoursesPage.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { getCourses } from "../api/courseApi";
 import CourseList from "./CourseList";
-const CoursesPage = () => {
+
+function CoursesPage() {
   const [courses, setCourses] = useState([]);
 
   useEffect(() => {
-    getCourses().then((_courses) => setCourses(_courses));
+    getCourses().then(_courses => setCourses(_courses));
   }, []);
 
   return (
@@ -14,45 +15,6 @@ const CoursesPage = () => {
       <CourseList courses={courses} />
     </>
   );
-};
-
-// class CoursesPage extends React.Component {
-//   state = {
-//     courses: [],
-//   };
-
-//   async componentDidMount() {
-//     this.setState({ courses: await getCourses() });
-//     // getCourses().then(courses => this.setState({ courses: courses }));
-//   }
-
-//   renderRow(course) {
-//     return (
-//       <tr key={course.id}>
-//         <td>{course.title}</td>
-//         <td>{course.authorId}</td>
-//         <td>{course.category}</td>
-//       </tr>
-//     );
-//   }
-
-//   render() {
-//     return (
-//       <>
-//         <h2>Courses</h2>
-//         <table className="table">
-//           <thead>
-//             <tr>
-//               <th>Title</th>
-//               <th>Author ID</th>
-//               <th>Category</th>
-//             </tr>
-//           </thead>
-//           <tbody>{this.state.courses.map(this.renderRow)}</tbody>
-//         </table>
-//       </>
-//     );
-//   }
-// }
+}
 
 export default CoursesPage;
